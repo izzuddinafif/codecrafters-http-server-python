d99c8c9faf5b936479b605e477d1c45585948110
afif <izzuddinafif@gmail.com>
2024-07-22 16:29:29 +0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,8 +9,25 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") # wait for client
-
+    # this is a concise way to do it:
+    # server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") # take the client socket object (index 0) and use the sendall() on that object
+    client, address = server_socket.accept()
+    data = client.recv(1024)
+    print(type(data))
+    print(data)
+    request = data.decode()
+    print(type(request))
+    print(request)
+    lines = request.split("\n")
+    print(type(lines))
+    print(lines)
+    req_line = lines[0].split(' ')
+    path = req_line[1]
+    print(path)
+    if(path == "/"):
+        client.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
