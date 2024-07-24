aa0ad2137c1925bd8f2117f9602b71c6a6c7270f
afif <izzuddinafif@gmail.com>
2024-07-24 09:20:08 +0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-# Uncomment this to pass the first stage
 import socket
 import threading
+import argparse
+import os
 
-
-def handle_client(client):
+def handle_client(client, directory):
     try:
         data = client.recv(1024).decode()
         if not data:
@@ -30,6 +30,23 @@ def handle_client(client):
                     user_agent = header[len("User-Agent: "):]
                     ua_length = len(user_agent)
                     client.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {ua_length}\r\n\r\n{user_agent}".encode())    
+        
+        elif path.startswith("/files/"):
+            filename = path[len("/files/"):]
+            filepath = os.path.join(directory, filename)
+
+            if os.path.isfile(filepath):
+                try:
+                    with open(filepath, 'r') as f:
+                        content = f.read()
+                        content_length = len(content)
+                        
+                        client.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {content_length}\r\n\r\n{content}".encode())
+                        
+                except Exception as e:
+                    print(f"Error opening file: {e}")
+                    client.sendall(b"HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n")
+
         else:
             client.sendall(b"HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n")
 
@@ -41,14 +58,15 @@ def handle_client(client):
         client.close()
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    
+    print("This is my own HTTP Server!, it is up and running :D")
+    parser = argparse.ArgumentParser(description="A simple HTTP Server") 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    
+    parser.add_argument('-d', '--directory', type=str, default=os.getcwd(), help="Directory to serve the file from")  
+    args = parser.parse_args()
+
     while True:
         client, address = server_socket.accept()
-        client_thread = threading.Thread(target=handle_client, args=[client])
+        client_thread = threading.Thread(target=handle_client, args=[client, args.directory])
         client_thread.start()
 
 
